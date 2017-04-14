1e8a9de9091420ffaccf22f83fd50d547fc839d0
vladko456 <photographervm1997@gmail.com>
2017-04-14 20:56:47 +0300

    Create MainActivity

---
+++ app/src/main/java/com/thecachemind/remindme/MainActivity.java
@@ -0,0 +1,18 @@
+package com.thecachemind.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Владислав on 11.04.2017.
+ */
+public class MainActivity extends Activity
+
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+    }
+}
